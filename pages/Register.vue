329ecb1706e90b81db269ca42af5fb11a69d78c4
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-10-04 16:51:26 +0200

    register

--- pages/Register.vue
+++ pages/Register.vue
@@ -17,13 +17,19 @@ export default {
       if (this.password === this.password_confirmation) {
         // register request
         try {
-          await this.$axios.get("http://localhost:8000/sanctum/csrf-cookie");
           let res = await this.$axios
             .post("http://localhost:8000/api/register", {
               name: this.name,
               email: this.email,
               password: this.password,
               password_confirmation: this.password_confirmation,
+            }).then(res => {
+              this.$auth.loginWith('laravelSanctum', {
+                data: {
+                  email: this.email,
+                  password: this.password,
+                }
+              });
             });
           this.$router.push('/');
         } catch (error) {
